367a5012b8a29b3fd8a085cf5e7c5d1f6a416afa
YOUR NAME <adien330@yahoo.com>
2019-08-27 18:40:40 -0400

    login/logout

--- capstone_vue_app/src/main.js
+++ capstone_vue_app/src/main.js
@@ -3,12 +3,17 @@ import App from './App.vue'
 import router from './router'
 import axios from "axios";
 
-axios.defaults.baseURL = 
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+axios.defaults.baseURL =
+    process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+
+var jwt = localStorage.getItem("jwt");
+if (jwt) {
+    axios.defaults.headers.common["Authorization"] = "Bearer " + jwt;
+}
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
