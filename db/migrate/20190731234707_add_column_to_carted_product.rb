b0eed5c671b398dc7d33c4d9a4b4fb921f79bd0c
YOUR NAME <adien330@yahoo.com>
2019-08-27 00:48:26 -0400

    workflow to have heroku db:migrate to work

--- db/migrate/20190731234707_add_column_to_carted_product.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToCartedProduct < ActiveRecord::Migration[5.2]
-  def change
-    add_column :carted_products, :comment, :text
-  end
-end
---
+++ db/migrate/20190827043248_remove_comment_column.rb
@@ -0,0 +1,4 @@
+class RemoveCommentColumn < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
